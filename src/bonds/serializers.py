5ea2b5773729b373ae6cba6b177754469ee82622
Abdul Rehman <abdulrehmanmirza135@gmail.com>
2022-04-08 11:37:30 -0700

    Initial commit

---
+++ src/bonds/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Bonds
+
+class BondsSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        bonds = Bonds.objects.create(**validated_data)
+        return bonds
+
+    class Meta:
+        model = Bonds
+        fields = ['id', 'bond_name', 'number_of_bonds', 'selling_price_mxn', 'user_id', 'status', 'buyer_id']
+
+
+class create_bond(serializers.ModelSerializer):
+    class Meta:
+        model = Bonds
+        fields = ['bond_name', 'number_of_bonds', 'selling_price_mxn']
